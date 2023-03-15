62e1efba44bfc168d1d31bfde5cf07f5860605e9
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-15 18:03:29 +0530

    Delete formula 3 (x+y)^3.cpp

--- formula 3 (x+y)^3.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int a,b,c;//x=a,y=b
-	printf("Enter a value for a & b= ");
-	scanf("%d%d",&a,&b);
-	c=(a*3)+(b*3)-(3*a*2*b)+(b*3);
-	printf("The sum of a & b = %d",c);
-}
